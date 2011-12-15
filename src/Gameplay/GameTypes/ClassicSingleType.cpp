7a5dfde5d8f9ba56795ead205b3d47ddf5353524
Cmaranec <cmaranec@seznam.cz>
2011-12-15 20:57:42 +0100

    Improved bomb reach calculation code
    Now should detonate boxes correctly

--- src/Gameplay/GameTypes/ClassicSingleType.cpp
+++ src/Gameplay/GameTypes/ClassicSingleType.cpp
@@ -46,31 +46,46 @@ void ClassicSingleGameType::OnBombBoom(BombRecord* bomb)
         pMap->DestroyDynamicRecords(bomb->x, bomb->y, DYNAMIC_TYPE_BOMB);
 
         // Tohle bude v budoucnu promenliva hodnota, podle bonusu apod.
-        uint32 bombreach = 2;
+        uint32 bombreach = 1;
 
         // Projdeme vsechny mozne zaznamy na mape az na maximalni definovany dosah
         // pokud tam neco je, zamezime dalsimu prepsani a zapiseme
-        uint32 reach_x1 = bombreach, reach_x2 = bombreach, reach_y1 = bombreach, reach_y2 = bombreach;
+        uint32 reach_x1 = bombreach+1, reach_x2 = bombreach+1, reach_y1 = bombreach+1, reach_y2 = bombreach+1;
+        // Promenna urcujici, zdali doslo ke kolizi kvuli objektu (true) nebo kvuli dosahu (false)
+        bool rx1_box = false, rx2_box = false, ry1_box = false, ry2_box = false;
+
         for (uint32 i = 0; i < bombreach; i++)
         {
             // Pokud jeste nebylo zapsano
-            if (reach_x1 == bombreach)
+            if (reach_x1 == bombreach+1)
                 // Overime, zdali na dane pozici neco neprostupneho je
-                if (pMap->IsDynamicRecordPresent(bomb->x + i, bomb->y, DYNAMIC_TYPE_BOX) || pMap->GetStaticRecord(bomb->x+i,bomb->y) == TYPE_SOLID_BOX)
+                if (pMap->IsDynamicRecordPresent(bomb->x + i + 1, bomb->y, DYNAMIC_TYPE_BOX) || pMap->GetStaticRecord(bomb->x+i+1,bomb->y) == TYPE_SOLID_BOX)
+                {
                     // Pokud ano, zamezime zapsanim maximalni pozice dalsimu prepsani
-                    reach_x1 = i;
+                    reach_x1 = i+1;
+                    rx1_box = true;
+                }
 
-            if (reach_x2 == bombreach)
-                if (pMap->IsDynamicRecordPresent(bomb->x - i, bomb->y, DYNAMIC_TYPE_BOX) || pMap->GetStaticRecord(bomb->x - i,bomb->y) == TYPE_SOLID_BOX)
-                    reach_x2 = i;
+            if (reach_x2 == bombreach+1)
+                if (pMap->IsDynamicRecordPresent(bomb->x - i - 1, bomb->y, DYNAMIC_TYPE_BOX) || pMap->GetStaticRecord(bomb->x - i - 1,bomb->y) == TYPE_SOLID_BOX)
+                {
+                    reach_x2 = i+1;
+                    rx2_box = true;
+                }
 
-            if (reach_y1 == bombreach)
-                if (pMap->IsDynamicRecordPresent(bomb->x, bomb->y + i, DYNAMIC_TYPE_BOX) || pMap->GetStaticRecord(bomb->x,bomb->y+i) == TYPE_SOLID_BOX)
-                    reach_y1 = i;
+            if (reach_y1 == bombreach+1)
+                if (pMap->IsDynamicRecordPresent(bomb->x, bomb->y + i + 1, DYNAMIC_TYPE_BOX) || pMap->GetStaticRecord(bomb->x,bomb->y+i+1) == TYPE_SOLID_BOX)
+                {
+                    reach_y1 = i+1;
+                    ry1_box = true;
+                }
 
-            if (reach_y2 == bombreach)
-                if (pMap->IsDynamicRecordPresent(bomb->x, bomb->y - i, DYNAMIC_TYPE_BOX) || pMap->GetStaticRecord(bomb->x,bomb->y-i) == TYPE_SOLID_BOX)
-                    reach_y2 = i;
+            if (reach_y2 == bombreach+1)
+                if (pMap->IsDynamicRecordPresent(bomb->x, bomb->y - i - 1, DYNAMIC_TYPE_BOX) || pMap->GetStaticRecord(bomb->x,bomb->y-i-1) == TYPE_SOLID_BOX)
+                {
+                    reach_y2 = i+1;
+                    ry2_box = true;
+                }
         }
         // Pak vsechno dekrementujeme - zapise se vzdy pozice uz nepristupneho pole
         --reach_x1;
@@ -91,10 +106,11 @@ void ClassicSingleGameType::OnBombBoom(BombRecord* bomb)
         templ = BillboardDisplayListRecord::Create(31, 0, 0, 0, 0.8f, 0.8f, true, true);
         sParticleEmitterMgr->AddEmitter(templ, bomb->x-0.5f, 0.1f, bomb->y-0.5f, 0.15f, 0.4f, 270.0f, 0, 0, 0, reach_x1*100, 20, 10.0f, 0.1f, 50, 10, 0, 0, 1500);
 
-        // A nakonec znicime vsechny bedny v dosahu bomby (+1 / -1 proto, ze reach je definovan pro dosah plamene. Bedna je o jedno pole dal)
-        pMap->DestroyDynamicRecords(bomb->x + reach_x1 + 1, bomb->y, DYNAMIC_TYPE_BOX);
-        pMap->DestroyDynamicRecords(bomb->x - reach_x1 - 1, bomb->y, DYNAMIC_TYPE_BOX);
-        pMap->DestroyDynamicRecords(bomb->x, bomb->y + reach_y1 + 1, DYNAMIC_TYPE_BOX);
-        pMap->DestroyDynamicRecords(bomb->x, bomb->y - reach_y2 - 1, DYNAMIC_TYPE_BOX);
+        // A nakonec znicime vsechny bedny v dosahu bomby (v pripade true hodnoty bool promenne +1 / -1 proto, ze
+        // reach je definovan pro dosah plamene. Bedna je o jedno pole dal)
+        pMap->DestroyDynamicRecords(bomb->x + reach_x1 + (rx1_box?1:0), bomb->y, DYNAMIC_TYPE_BOX);
+        pMap->DestroyDynamicRecords(bomb->x - reach_x2 - (rx2_box?1:0), bomb->y, DYNAMIC_TYPE_BOX);
+        pMap->DestroyDynamicRecords(bomb->x, bomb->y + reach_y1 + (ry1_box?1:0), DYNAMIC_TYPE_BOX);
+        pMap->DestroyDynamicRecords(bomb->x, bomb->y - reach_y2 - (rx1_box?1:0), DYNAMIC_TYPE_BOX);
     }
 }
