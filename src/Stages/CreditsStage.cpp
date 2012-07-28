50397f7dd35722489c27c44c4dd6f900f4b097af
cmaranec <cmaranec@seznam.cz>
2012-07-28 15:39:36 +0200

    Adjust credits stage - delete music, which shouldn't be there

--- src/Stages/CreditsStage.cpp
+++ src/Stages/CreditsStage.cpp
@@ -26,8 +26,8 @@ void CreditsStage::OnDraw()
     }
 
     // Limitace na maximalni bod posunu
-    if (m_scrollPos > 440+400+120)
-        m_scrollPos = 440+400+120;
+    if (m_scrollPos > 440+370+120)
+        m_scrollPos = 440+370+120;
 
     sDisplay->PrintText(FONT_ONE, 0, 80-m_scrollPos, FONT_SIZE_1, TEXT_FLAG_CENTERED_X, NOCOLOR, "Bomberman 3D");
 
@@ -47,21 +47,20 @@ void CreditsStage::OnDraw()
 
     sDisplay->PrintText(FONT_ONE, 0, 170-m_scrollPos+320+60, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 127), "Hudba:");
     sDisplay->PrintText(FONT_ONE, 0, 200-m_scrollPos+320+60, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 255), "1024mb");
-    sDisplay->PrintText(FONT_ONE, 0, 230-m_scrollPos+320+60, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 255), "Mirrored Theory");
-    sDisplay->PrintText(FONT_ONE, 0, 260-m_scrollPos+320+60, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 255), "Sinetrium");
+    sDisplay->PrintText(FONT_ONE, 0, 230-m_scrollPos+320+60, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 255), "Sinetrium");
 
-    sDisplay->PrintText(FONT_ONE, 0, 170-m_scrollPos+400+120, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 127), "Zvláštní podìkování:");
-    sDisplay->PrintText(FONT_ONE, 0, 200-m_scrollPos+400+120, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 255), "Tomáš Kleisner");
-    sDisplay->PrintText(FONT_ONE, 0, 230-m_scrollPos+400+120, FONT_SIZE_4, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 0), "(nápad, poskytnutí zkušebny pro nahrávání zvukù)");
-    sDisplay->PrintText(FONT_ONE, 0, 260-m_scrollPos+400+120, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 255), "Freghar");
-    sDisplay->PrintText(FONT_ONE, 0, 290-m_scrollPos+400+120, FONT_SIZE_4, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 0), "(pomoc pøi optimalizaci a algoritmizaci)");
-    sDisplay->PrintText(FONT_ONE, 0, 320-m_scrollPos+400+120, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 255), "Jakub Horák");
-    sDisplay->PrintText(FONT_ONE, 0, 350-m_scrollPos+400+120, FONT_SIZE_4, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 0), "(testování)");
-    sDisplay->PrintText(FONT_ONE, 0, 380-m_scrollPos+400+120, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 255), "Martin Tomášek");
-    sDisplay->PrintText(FONT_ONE, 0, 410-m_scrollPos+400+120, FONT_SIZE_4, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 0), "(pomoc pøi ladìní vzhledu a hratelnosti)");
+    sDisplay->PrintText(FONT_ONE, 0, 170-m_scrollPos+370+120, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 127), "Zvláštní podìkování:");
+    sDisplay->PrintText(FONT_ONE, 0, 200-m_scrollPos+370+120, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 255), "Tomáš Kleisner");
+    sDisplay->PrintText(FONT_ONE, 0, 230-m_scrollPos+370+120, FONT_SIZE_4, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 0), "(nápad, poskytnutí zkušebny pro nahrávání zvukù)");
+    sDisplay->PrintText(FONT_ONE, 0, 260-m_scrollPos+370+120, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 255), "Freghar");
+    sDisplay->PrintText(FONT_ONE, 0, 290-m_scrollPos+370+120, FONT_SIZE_4, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 0), "(pomoc pøi optimalizaci a algoritmizaci)");
+    sDisplay->PrintText(FONT_ONE, 0, 320-m_scrollPos+370+120, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 255), "Jakub Horák");
+    sDisplay->PrintText(FONT_ONE, 0, 350-m_scrollPos+370+120, FONT_SIZE_4, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 0), "(testování)");
+    sDisplay->PrintText(FONT_ONE, 0, 380-m_scrollPos+370+120, FONT_SIZE_3, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 255), "Martin Tomášek");
+    sDisplay->PrintText(FONT_ONE, 0, 410-m_scrollPos+370+120, FONT_SIZE_4, TEXT_FLAG_CENTERED_X, COLOR(255, 127, 0), "(pomoc pøi ladìní vzhledu a hratelnosti)");
 
     // Logo skoly - jakmile bude uprostred pohledu, zastavi se skrolovani
-    sDisplay->Draw2D(70, (WIDTH/2)-70, 440+400+120-m_scrollPos+HEIGHT/2-70, 140, 140);
+    sDisplay->Draw2D(70, (WIDTH/2)-70, 440+370+120-m_scrollPos+HEIGHT/2-70, 140, 140);
 }
 
 void CreditsStage::OnKeyStateChange(uint16 key, bool press)
